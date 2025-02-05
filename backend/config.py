206561bc65749052d76484e32b771c4ed8fc778b
vantiger69 <vantiger817@gmail.com>
2025-02-05 18:42:41 +0300

    database addition for render

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    database_path = '/tmp/database.db'  # Use /tmp for Render compatibility
+
+    database_path = os.path.join('/tmp', 'database.db')
+    
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'sqlite:///{database_path}')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ENV = os.getenv('FLASK_ENV', 'development')
