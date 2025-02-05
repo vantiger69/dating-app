027c7726a1717635b6df6a5f88ea461066491613
vantiger69 <vantiger817@gmail.com>
2025-02-05 21:10:08 +0300

    changes in the database for rendering

--- backend/config.py
+++ backend/config.py
@@ -2,8 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-
-    database_path = os.path.join('/tmp', 'database.db')
+    
+    # Use relative path for local development and /tmp for production
+    database_path = os.path.join(os.path.dirname(__file__), 'database.db')  # Local development
+    # database_path = os.path.join('/tmp', 'database.db')  # For Render or production environments
     
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'sqlite:///{database_path}')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
